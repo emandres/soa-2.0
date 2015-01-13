6b83cdb8870a3ba722c38296088c5fcd53441055
Eric Andres <emandres@gmail.com>
2015-01-13 10:07:53 -0700

    Initial commit

---
+++ pizza.js
@@ -0,0 +1,79 @@
+/*
+  ip: 54.149.117.183
+  username: craftsman
+  password: utahsc2015
+
+  pizzarequested.v1
+  {
+    "Name": "",
+    "Address": "",
+    "Toppings": [""]
+    }
+*/
+
+var uuid = require('uuid'),
+    amqp = require('amqp');
+
+var connection = amqp.createConnection({
+	host: '54.149.117.183',
+	login: 'craftsman',
+	password: 'utahsc2015'
+});
+
+connection.on('error', function(err) {
+	console.error(err);
+})
+
+var orderId = uuid.v4();
+var orderPizza = function(exchange, coupon) {
+	console.log('got the exchange!');
+	var pizzaOrder = {
+		Name: "Eric & Allan",
+		Address: "555 Main St.",
+		Toppings: ["Pizza", "Yo dawg!"],
+		OrderId: orderId
+	};
+	if (coupon) {
+		pizzaOrder.Coupon = coupon;
+	}
+
+	exchange.publish('', JSON.stringify(pizzaOrder), {}, pizzaOrdered);
+};
+
+var pizzaOrdered = function(err) {
+	if (err) {
+		console.error("error: " + err);
+	}
+	else {
+		console.log("message published");
+	}
+};
+
+connection.on('ready', function() {
+	console.log("Connected to rabbit!");
+
+	var exchangeOptions = {
+		type: 'fanout',
+		durable: true,
+		autoDelete: false,
+		confirm: true
+	};
+
+	connection.exchange('pizzarequested.v1', exchangeOptions, function(pizzaRequestedExchange) {
+		orderPizza(pizzaRequestedExchange);
+
+		connection.exchange('couponissued.v1', exchangeOptions, function(couponIssuedExchange) {
+			connection.queue('couponissued.v1.eric-and-allan', {}, function(queue) {
+				queue.bind(couponIssuedExchange, '');
+				queue.subscribe(function(message) {
+					console.log(message);
+					var couponMessage = JSON.parse(message);
+					if (couponMessage.OrderId === orderId) {
+						orderPizza(pizzaRequestedExchange, couponMessage.Coupon)
+					}
+				})
+			});
+		});
+	});
+	
+});
